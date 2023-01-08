bdbf5f8303d1c67f62ca5f7a701cc684df915997
Toonlink3000 <dannyprague@seznam.cz>
2023-01-08 14:03:49 +0100

    added simple website for github pages

--- src/main.py
+++
@@ -1,7 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(
-	prog = 'GroupListen', 
-	description = 'Divert audio from one audio device to another on multiple devices at the same time ', 
-	epilog = 'Made by Daniel Adam Coats in 2022')
-
